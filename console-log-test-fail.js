b6cbc9057b534ffa27e2c2469aee64872fd4205a
Kerem <keremgerede81@gmail.com>
2026-07-03 00:40:11 +0300

    console log hatası çözümlü

--- console-log-test-fail.js
+++ console-log-test-fail.js
@@ -3,16 +3,12 @@ export function calculateOrderTotal(items) {
         return sum + item.price * item.quantity;
     }, 0);
 
-    console.log("Calculated order total:", total);
-
     return total;
 }
 
 export function applyDiscount(total, discountRate) {
     const discountAmount = total * discountRate;
     const finalTotal = total - discountAmount;
-
-    console.log("Final total after discount:", finalTotal);
 
     return finalTotal;
 }
